e71c62a5bfc6461ed79512fa91e2b23fcdbf6637
Elena Sacristán Sacristán <44655750+elenasacristan@users.noreply.github.com>
2020-10-29 16:55:31 +0000

    Display activity cards using bootstrap grid

--- frontend/src/components/activities/activities.js
+++ frontend/src/components/activities/activities.js
@@ -1,12 +1,24 @@
 import React from "react";
 import "./activities.css";
 import Activity from "../activity/activity";
+import Pedagogico from "../../common/images/Pedagogicos.png";
 
 const Activities = () => {
   return (
-    <div className="activities">
-      <Activity />
-      <Activity />
+    <div class="activities container">
+      <h2 className="font-weight-light text-center text-lg-left mt-4 mb-0">
+        Conciertos
+      </h2>
+
+      <hr class="mt-2 mb-5" />
+      <div class="row text-center">
+        <Activity />
+        <Activity />
+        <Activity />
+        <Activity />
+        <Activity />
+        <Activity />
+      </div>
     </div>
   );
 };
--- frontend/src/components/activity/activity.js
+++ frontend/src/components/activity/activity.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "./activity.css";
+import Pedagogico from "../../common/images/Pedagogicos.png";
 
 const Activity = () => {
   return (
-    <div className="acivity-container d-flex flex-column justify-content-between 
-        align-items-center mb-5">
-      <h2 className="mt-4">Pedagógicos</h2>
-      <button className="btn btn-lg mb-4">Detalles</button>
+    <div class="col-lg-4 col-md-6 col-12">
+      <div className="d-block mb-4 h-100 acivity-container">
+        <img class="img-fluid" src={Pedagogico} />
+        <h2 className="mt-4 mx-auto">Pedagogico</h2>
+        <button className="btn btn-lg mb-4">Detalles</button>
+      </div>
     </div>
   );
 };
