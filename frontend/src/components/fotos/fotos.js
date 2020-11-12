362f520e30b4b1fe50dda801d6e1e70eac5e6d6a
Elena Sacristán Sacristán <44655750+elenasacristan@users.noreply.github.com>
2020-11-12 15:32:58 +0000

    remove duplicated code and update class with className

--- frontend/src/components/fotos/fotos.js
+++ frontend/src/components/fotos/fotos.js
@@ -4,12 +4,12 @@ import Foto from "../foto/foto";
 
 const Fotos = () => {
   return (
-    <div class="fotos container px-2">
+    <div className="fotos container px-2">
       <h2 className="font-weight-light text-center text-lg-left mt-4 mb-5">
         Fotos
       </h2>
 
-      <div class="d-flex flex-wrap text-center">
+      <div className="d-flex flex-wrap text-center">
         {[1, 2, 3, 4,5,6,7,8].map((number, index) => (
           index % 2 === 0 ?
             <Foto rotate={"right"}/> :
