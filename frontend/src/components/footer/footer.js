ec0b9d1f6acb175b0b7328048aa5b95df6e35b70
Elena Sacristán Sacristán <44655750+elenasacristan@users.noreply.github.com>
2020-11-12 15:19:00 +0000

    Add footer

---
+++ frontend/src/components/footer/footer.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer p-4">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6 text-center text-md-left pb-2 pb-md-0">
+            <h6 className="mb-4">Contacto:</h6>
+            <p>
+              <span className="d-none d-md-inline">E-mail: </span>musicalatte.paratodos@gmail.com
+              <br />
+              <span  className="d-none d-md-inline">Teléfono: </span>(+34) 676 92 42 85
+            </p>
+          </div>
+          <div className="col-md-6 d-flex justify-content-center justify-content-md-end align-items-center">
+            <a href="#" target="_blank" rel="noopener noreferrer">
+              <i className="fa fa-envelope"></i>
+            </a>
+            <a href="#" target="_blank" rel="noopener noreferrer">
+              <i className="fa fa-facebook-official pl-4"></i>
+            </a>
+            <a href="#" target="_blank" rel="noopener noreferrer">
+              <i className="fa fa-instagram pl-4"></i>
+            </a>
+            <a href="#" target="_blank" rel="noopener noreferrer">
+              <i className="fa fa-youtube pl-4"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
