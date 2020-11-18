16f286bb5df0961563d7d565a9cb2d4bbce1dccd
Elena Sacristán Sacristán <44655750+elenasacristan@users.noreply.github.com>
2020-11-18 17:56:17 +0000

    update styles inicio

--- frontend/src/components/hacemos-item/Hacemos_item.js
+++ frontend/src/components/hacemos-item/Hacemos_item.js
@@ -7,7 +7,7 @@ const Hacemos_item = ({ item }) => {
     <div className="col-sm-4 col-lg-2 d-flex flex-column mx-auto align-items-center mb-4 mb-md-0">
       <i className={`fa ${item.logo_name}`}></i>
       <h5 className="my-2">{item.titulo}</h5>
-      <Link className="btn btn-sm pt-2 pb-0" to={`/actividades/${item.titulo}`}>{item.text_boton}</Link>
+      <Link className="btn btn-sm pt-0 pb-0" to={`/actividades/${item.titulo}`}>{item.text_boton}</Link>
     </div>
   );
 };
