db531205596220b33c8e35699d7f139d54038af8
Elena Sacristán Sacristán <44655750+elenasacristan@users.noreply.github.com>
2020-11-20 17:42:57 +0000

    update DRF permissions

--- backend/contacto/api/views.py
+++ backend/contacto/api/views.py
@@ -1,7 +1,15 @@
 from rest_framework import viewsets
 from .serializers import ContactoSerializer
 from ..models import Contacto
+from rest_framework.permissions import AllowAny
 
 class ContactoViewSet(viewsets.ModelViewSet):
+    '''
+    We will allow anyone to do post request but they won't be able to see 
+    other emails becase I'm only allowing the post method
+    '''
+    permission_classes = [AllowAny]
+    http_method_names = ['post']
     queryset = Contacto.objects.all().order_by('-fecha')
     serializer_class = ContactoSerializer
+
