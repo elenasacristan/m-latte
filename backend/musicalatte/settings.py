a5f8a20d54055124af34bc4ed808584f94df2bfc
Elena Sacristán Sacristán <44655750+elenasacristan@users.noreply.github.com>
2020-11-20 17:52:48 +0000

    Add whitenoise

--- backend/musicalatte/settings.py
+++ backend/musicalatte/settings.py
@@ -68,6 +68,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'musicalatte.urls'
@@ -140,7 +141,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'     
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'build/static')] 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
