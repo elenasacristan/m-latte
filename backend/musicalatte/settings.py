48d369c522f1bfe1c14e7d0cc16fcbe3e565f864
Elena Sacristán Sacristán <44655750+elenasacristan@users.noreply.github.com>
2020-11-20 18:03:22 +0000

    Add postgress database

--- backend/musicalatte/settings.py
+++ backend/musicalatte/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
+import dj_database_url
 from pathlib import Path
 
 import cloudinary
@@ -30,13 +31,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'k__u#gf@6249y5hw9(ssidr%ezrx+9xjus*68v0imt1=e73y7%'
+# SECRET_KEY = 'k__u#gf@6249y5hw9(ssidr%ezrx+9xjus*68v0imt1=e73y7%'
+SECRET_KEY =  os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -95,11 +96,15 @@ WSGI_APPLICATION = 'musicalatte.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
 
