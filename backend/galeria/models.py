c087131744b8ede822058b24dc4dce1c41c3b2a3
Elena Sacristán Sacristán <44655750+elenasacristan@users.noreply.github.com>
2020-11-21 16:49:10 +0000

    update foto model

--- backend/galeria/models.py
+++ backend/galeria/models.py
@@ -4,7 +4,7 @@ from cloudinary.models import CloudinaryField
 class Foto(models.Model):
     year = models.IntegerField(null=True, blank=True)
     lugar = models.CharField(max_length=120, null=True, blank=True)
-    titulo =  models.CharField(max_length=120, null=True, blank=True)
+    titulo =  models.CharField(max_length=400, null=True, blank=True)
     foto = CloudinaryField('foto', null=True, blank=True)
 
     def __str__(self):
