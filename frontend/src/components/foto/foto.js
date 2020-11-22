eda4a53ae21dbb713a46b1ad041cfd90df90ed9c
Elena Sacristán Sacristán <44655750+elenasacristan@users.noreply.github.com>
2020-11-22 15:54:23 +0000

    Improve display of images while loading

--- frontend/src/components/foto/foto.js
+++ frontend/src/components/foto/foto.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./foto.css";
 
 const Foto = ({ rotate, foto }) => {
+  const [loaded, setLoaded] = useState(false);
+
   return (
     <div className={`mb-1 mb-lg-5 foto-container mx-auto ${rotate}`}>
-      <img src={`https://res.cloudinary.com/dm3k4mri1/${foto.foto}`} />
-      <h6 className="mt-4 text-center">
-      {foto.titulo}
-      </h6>
+      {loaded ? null : (
+        <div
+          style={{
+            background: "white",
+            height: "400px",
+            width: "400px",
+          }}
+        />
+      )}
+      <img
+        style={loaded ? {} : { display: "none" }}
+        src={`https://res.cloudinary.com/dm3k4mri1/${foto.foto}`}
+        onLoad={() => setLoaded(true)}
+      />
+      <h6 className="mt-4 text-center">{foto.titulo}</h6>
     </div>
   );
 };
