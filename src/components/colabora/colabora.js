ceb28eb1bed50f38dacb8aa0f0e834185e1e2386
Elena Sacristán Sacristán <44655750+elenasacristan@users.noreply.github.com>
2020-11-24 20:14:27 +0000

    Update media queries breakpoints and colours

--- src/components/colabora/colabora.js
+++ src/components/colabora/colabora.js
@@ -20,11 +20,11 @@ const Colabora = () => {
       <div className="colabora mx-auto px-4 px-md-5 pb-5">
         <div className="mx-auto text-center">
           <h2 className="main-title title mt-4 mb-5">{colabora.titulo}</h2>
-          <div class="row d-flex align-items-center">
-            <div class="col-lg-5 d-flex justify-content-center d-lg-block">
+          <div className="row d-flex align-items-center">
+            <div className="col-lg-5 d-flex justify-content-center d-lg-block">
               <img src={colaborate}></img>
             </div>
-            <div class="mt-5 mt-lg-0 col-lg-6 mx-auto">
+            <div className="mt-5 mt-lg-0 col-lg-6 mx-auto">
               <p>{colabora.texto1}</p>
               <p>
                 <strong>{colabora.n_cuenta}</strong>
