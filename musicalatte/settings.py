e8ff3eba9bfad9d6f31a4e795590ca0e8d5e6ced
Elena Sacristán Sacristán <44655750+elenasacristan@users.noreply.github.com>
2020-11-25 10:52:49 +0000

    Add static STATICFILES_STORAGE to seettings and Debug=False

--- musicalatte/settings.py
+++ musicalatte/settings.py
@@ -41,7 +41,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY =  os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # ALLOWED_HOSTS = ['127.0.0.1', 'musicalatte.herokuapp.com']
 
@@ -154,6 +154,8 @@ USE_TZ = True
 STATIC_URL = '/static/'     
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'build/static')] 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
