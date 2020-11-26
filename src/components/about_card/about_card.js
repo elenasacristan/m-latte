fbc57147f39da3c55d48ba990c66259607c82b2c
Elena Sacristán Sacristán <44655750+elenasacristan@users.noreply.github.com>
2020-11-26 19:14:35 +0000

    refactor code about_card and hacemos_item

--- src/components/about_card/about_card.js
+++ src/components/about_card/about_card.js
@@ -4,7 +4,7 @@ import "./about_card.css";
 const AboutCard = ({ item }) => {
   return (
     <div className="col-md-4" data-aos="flip-up">
-      <div className="about-card-container d-flex flex-column mx-auto mb-4 mb-md-0">
+      <div className="about-card-container d-flex flex-column mx-auto mb-4 mb-md-0 p-3">
         <div className="d-flex align-items-center justify-content-center pb-3">
           <i className={`fa ${item.logo_name}`}></i>
           <h4 className="ml-2">{item.titulo}</h4>
