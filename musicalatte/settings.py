26f57c3841b94abd4f439c76c5349c86db49eba9
Elena Sacristán Sacristán <44655750+elenasacristan@users.noreply.github.com>
2020-11-26 09:39:55 +0000

    Debug=True

--- musicalatte/settings.py
+++ musicalatte/settings.py
@@ -41,7 +41,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY =  os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 # ALLOWED_HOSTS = ['desktop-m46m9jt','127.0.0.1']
 # ALLOWED_HOSTS = []
 
