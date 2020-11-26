dd42e83cc8e48a4d88206e36b0bf3d4f9d4cc6fc
Elena Sacristán Sacristán <44655750+elenasacristan@users.noreply.github.com>
2020-11-26 20:21:01 +0000

    Refactor code discografia

--- src/components/discografia/discografia.js
+++ src/components/discografia/discografia.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from "react";
 import "./discografia.css";
 import Disco from "../disco/disco";
-import Aos from 'aos';
-import 'aos/dist/aos.css';
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 const Discografia = () => {
   const [discos, setDiscos] = useState([]);
-  const [titleDiscografia, setTitleDiscografia] = useState('');
+  const [titleDiscografia, setTitleDiscografia] = useState("");
 
   useEffect(() => {
     fetch("/api/sobremi/disco/")
@@ -14,36 +14,36 @@ const Discografia = () => {
       .then((data) => {
         setDiscos(data);
       });
-      fetch("/api/sobremi/sobremi/")
+    fetch("/api/sobremi/sobremi/")
       .then((res) => res.json())
       .then((data) => {
         setTitleDiscografia(data[0].titulo3);
       });
-      Aos.init({ duration: 1000 });
+    Aos.init({ duration: 1000 });
   }, []);
-
 
   if (!titleDiscografia && discos.length < 1) {
     return "";
-  } else{
+  } else {
     return (
-      <div className="discografia mt-5">
-        <h4 className="text-center mb-5 title-discografia mt-lg-0 py-xl-0">{titleDiscografia}</h4>
-        <div className="timeline m-auto">
-          {discos.map((disco, index) =>
-            index % 2 === 0 ? (
-              <Disco key={disco.id} disco={disco} side="left"/>
-            ) : (
-              <Disco key={disco.id} disco={disco} side="right"/>
-            )
-          )}
+      <div className="col-12 offset-xl-1 col-xl-6 discografia-container">
+        <div className="discografia mt-5">
+          <h4 className="text-center mb-5 title-discografia mt-lg-0 pb-4">
+            {titleDiscografia}
+          </h4>
+          <div className="timeline m-auto">
+            {discos.map((disco, index) =>
+              index % 2 === 0 ? (
+                <Disco key={disco.id} disco={disco} side="left" />
+              ) : (
+                <Disco key={disco.id} disco={disco} side="right" />
+              )
+            )}
+          </div>
         </div>
       </div>
     );
   }
-
-
-  
 };
 
 export default Discografia;
