5f6d67c206f89264815362aa972a5cfe4cd3961f
Elena Sacristán Sacristán <44655750+elenasacristan@users.noreply.github.com>
2020-11-27 11:34:38 +0000

    Refactor conde NotFound

--- src/components/notfound/notfound.js
+++ src/components/notfound/notfound.js
@@ -5,7 +5,7 @@ import not_found from "../../common/images/not_found.png";
 const NotFound = () => {
   return (
     <div className="notfound text-center" id="about">
-        <h2 className="title m-auto w-75">No hemos encontrado la página</h2>
+        <h2 className="title main-title m-auto w-75">No hemos encontrado la página</h2>
         <img className="pt-5" alt="pagina no existe" src={not_found} />
     </div>
   );
