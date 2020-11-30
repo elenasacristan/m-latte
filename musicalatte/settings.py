8cadcce55deb26de8cfa463c1bc515e9a33a3770
Elena Sacristán Sacristán <44655750+elenasacristan@users.noreply.github.com>
2020-11-30 18:52:31 +0000

    Test without django-heroku

--- musicalatte/settings.py
+++ musicalatte/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
-import django_heroku
+# import django_heroku
 import dj_database_url
 from pathlib import Path
 
@@ -158,7 +158,7 @@ USE_TZ = True
 
 
 # Configure app for Heroku deployment
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
